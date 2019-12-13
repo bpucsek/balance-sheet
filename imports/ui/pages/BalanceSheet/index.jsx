159f33f430daec4820fa8433350f0e337a5d7ebc
Blake Pucsek <bpucsek@gmail.com>
2019-12-13 14:25:08 -0500

    Switch between mobile and desktop layout stubs

--- imports/ui/pages/BalanceSheet/index.jsx
+++ imports/ui/pages/BalanceSheet/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function({ device }) {
-  return (
-    <div>
-      {(device.isMobile) ? 'mobile' : 'desktop'}
-    </div>
-  );
+import DesktopLayout from '/imports/ui/pages/BalanceSheet/layouts/Desktop';
+import MobileLayout from '/imports/ui/pages/BalanceSheet/layouts/Mobile';
+
+export default function(props) {
+  if (props.device.isMobile) {
+    return (<MobileLayout { ...props } />);
+  } else {
+    return (<DesktopLayout { ...props } />);
+  }
 }
---
+++ imports/ui/pages/BalanceSheet/layouts/Mobile.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function({ device }) {
+  return (
+    <div>
+      MOBILE
+    </div>
+  );
+}
