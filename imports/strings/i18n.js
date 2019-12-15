d6d90af01d903b85bfb10682ff586faa5bf29121
Blake Pucsek <bpucsek@gmail.com>
2019-12-15 00:49:30 -0500

    Remove sprintf

--- imports/strings/i18n.js
+++ imports/strings/i18n.js
@@ -1,13 +1,10 @@
-import { sprintf } from 'sprintf-js';
-
 import en from './locales/en';
 
 // Note: This is just a really basic handler for i18n, which I think is a pretty good thing to take care of
 // as early as possible if there is a chance an app will need to support multiple locales.
 
-export default function(stringId, args={}) {
-  let { locale='en', ...rest } = args,
-    localization;
+export default function(stringId, locale='en') {
+  let localization;
 
   switch(locale) {
     case 'en':
@@ -15,5 +12,5 @@ export default function(stringId, args={}) {
       localization = en;
   }
 
-  return sprintf(localization[stringId] || localization['err.string-not-found'], rest);
+  return localization[stringId] || localization['err.string-not-found'];
 }
